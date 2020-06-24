a8e5116413dda097db4b723654a7a0f56116f428
drppsy <1213540281@qq.com>
2020-06-24 18:35:47 +0800

    编号

---
+++ 011recursion.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+# def fact(n):
+#     if n == 1:
+#         return 1
+#     else:
+#         return n*fact(n-1)
+#
+# def test_fact():
+#     a = fact(10)
+#     print(a)
+#
+# test_fact()
+
+class Stack():
+    def __init__(self):
+        self.stac = deque()
+
+    def push(self,n):
+        return self.stac.append(n)
+
+    def pop(self):
+        return self.stac.pop()
+
+    def is_empty(self):
+        return len(self.stac) == 0
+
+def print_num_use_stack(n):
+    s = Stack()
+    while n > 0:
+        s.push(n)
+        n -= 1
+
+    while not s.is_empty():
+        print(s.pop())
+
+print_num_use_stack(10)
