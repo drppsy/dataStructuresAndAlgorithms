e58dc5d2b591135f16cef40eecd8c3e336c312ff
drppsy <1213540281@qq.com>
2020-07-30 11:12:25 +0800

    字典的遍历

---
+++ learn_python/traverse_adict.py
@@ -0,0 +1,13 @@
+adict = {"aa":"11","bb":"22","cc":"33","dd":"44"}
+
+# 遍历字典的key
+for item in adict:
+    print(item)
+
+# 遍历字典的value
+for value in adict.values():
+    print(value)
+
+# 遍历字典的key和value
+for k,v in adict.items():
+    print(k,v)
